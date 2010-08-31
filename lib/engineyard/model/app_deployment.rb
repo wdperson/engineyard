afdf851f6b863a675d1828ae340395233af362cb
Andy Delcambre & Larry Diehl <adelcambre@engineyard.com>
2010-08-31 16:50:40 -0700

    WIP

--- lib/engineyard/model/app_deployment.rb
+++ lib/engineyard/model/app_deployment.rb
@@ -10,16 +10,42 @@ module EY
 
         candidates = filter_candidates(:account, options, candidates)
 
-        if options[:app_name]
-          candidates = filter_candidates(:app_name, options, candidates)
+        app_candidates =if options[:app_name]
+          filter_candidates(:app_name, options, candidates)
         elsif options[:repo]
-          candidates = candidates.select {|c| c.repo == options[:repo] }
+          candidates.select {|c| c.repo == options[:repo] }
         end
 
-        candidates = filter_candidates(:environment_name, options, candidates)
+        environment_candidates = filter_candidates(:environment_name, options, candidates)
+        candidates = app_candidates & environment_candidates
 
-        raise NoMatchesError if candidates.empty?
-        raise MultipleMatchesError if candidates.size > 1
+        if candidates.empty?
+          if environment_candidates.empty?
+            message = "Could not find any matching environments."
+          elsif app_candidates.empty?
+            message = "Could not find any matching applications."
+          else
+            message = "The matched apps & environments do not correspond with each other.\n"
+            message << "Applications:\n"
+            app_candidates.map{|ad| ad.app_name}.uniq.each do |app_name|
+              app = app_candidates.first.api.apps.named(app_name)
+              message << "\t#{app.name}\n"
+              app.environments.each do |env|
+                message << "\t\t#{env.name} # ey deploy -e #{env.name} -a #{app.name}\n"
+              end
+            end
+          end
+          raise NoMatchesError.new(message)
+        elsif candidates.size > 1
+          message = "Multiple app deployments possible, please be more specific:\n\n"
+          candidates.map{|c| c.app_name}.uniq.each do |app_name|
+            message << "#{app_name}\n"
+            candidates.select {|x| x.app_name == app_name }.map{|x| x.environment_name}.uniq.each do |env_name|
+              message << "\t#{env_name} # ey deploy -e #{env_name} -a #{app_name}\n"
+            end
+          end
+          raise MultipleMatchesError.new(message)
+        end
         candidates.first
       end
 
